e52cbbc942b52baba58d9d4fe8e33ef6cab7fcd1
Ruter <ruter.lu@gmail.com>
2016-04-13 14:03:53 +0800

    Add assets and set buttons

---
+++ GuessTheFlag/GuessTheFlag/ViewController.swift
@@ -0,0 +1,29 @@
+//
+//  ViewController.swift
+//  GuessTheFlag
+//
+//  Created by Ruter on 16/4/13.
+//  Copyright © 2016年 Ruter. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+  @IBOutlet weak var button1: UIButton!
+  @IBOutlet weak var button2: UIButton!
+  @IBOutlet weak var button3: UIButton!
+  
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    // Do any additional setup after loading the view, typically from a nib.
+  }
+
+  override func didReceiveMemoryWarning() {
+    super.didReceiveMemoryWarning()
+    // Dispose of any resources that can be recreated.
+  }
+
+
+}
+
