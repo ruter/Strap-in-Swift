228c387e912b96ceb2ecb2b0baa20903f68a2690
Ruter <ruter.lu@gmail.com>
2016-04-14 07:58:47 +0800

    Fix cell deselect problem

--- SocialMedia/SocialMedia/MasterViewController.swift
+++ SocialMedia/SocialMedia/MasterViewController.swift
@@ -1,8 +1,8 @@
 //
 //  MasterViewController.swift
-//  StormViewer
+//  SocialMedia
 //
-//  Created by Ruter on 16/4/12.
+//  Created by Ruter on 16/4/14.
 //  Copyright © 2016年 Ruter. All rights reserved.
 //
 
@@ -11,23 +11,19 @@ import UIKit
 class MasterViewController: UITableViewController {
 
   var detailViewController: DetailViewController? = nil
-  var objects = [String]()
+  var objects = [AnyObject]()
 
 
   override func viewDidLoad() {
     super.viewDidLoad()
     // Do any additional setup after loading the view, typically from a nib.
-    
-    self.title = "PicList"
-    
-    let fm = NSFileManager.defaultManager()
-    let path = NSBundle.mainBundle().resourcePath!
-    let items = try! fm.contentsOfDirectoryAtPath(path)
-    
-    for item in items {
-      if item.hasPrefix("nssl") {
-        objects.append(item)
-      }
+    self.navigationItem.leftBarButtonItem = self.editButtonItem()
+
+    let addButton = UIBarButtonItem(barButtonSystemItem: .Add, target: self, action: "insertNewObject:")
+    self.navigationItem.rightBarButtonItem = addButton
+    if let split = self.splitViewController {
+        let controllers = split.viewControllers
+        self.detailViewController = (controllers[controllers.count-1] as! UINavigationController).topViewController as? DetailViewController
     }
   }
 
@@ -41,18 +37,22 @@ class MasterViewController: UITableViewController {
     // Dispose of any resources that can be recreated.
   }
 
+  func insertNewObject(sender: AnyObject) {
+    objects.insert(NSDate(), atIndex: 0)
+    let indexPath = NSIndexPath(forRow: 0, inSection: 0)
+    self.tableView.insertRowsAtIndexPaths([indexPath], withRowAnimation: .Automatic)
+  }
+
   // MARK: - Segues
 
   override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
     if segue.identifier == "showDetail" {
         if let indexPath = self.tableView.indexPathForSelectedRow {
-            let object = objects[indexPath.row]
+            let object = objects[indexPath.row] as! NSDate
             let controller = (segue.destinationViewController as! UINavigationController).topViewController as! DetailViewController
             controller.detailItem = object
             controller.navigationItem.leftBarButtonItem = self.splitViewController?.displayModeButtonItem()
             controller.navigationItem.leftItemsSupplementBackButton = true
-          
-            self.tableView.deselectRowAtIndexPath(indexPath, animated: false)
         }
     }
   }
@@ -70,8 +70,8 @@ class MasterViewController: UITableViewController {
   override func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
     let cell = tableView.dequeueReusableCellWithIdentifier("Cell", forIndexPath: indexPath)
 
-    let object = objects[indexPath.row]
-    cell.textLabel!.text = object
+    let object = objects[indexPath.row] as! NSDate
+    cell.textLabel!.text = object.description
     return cell
   }
 
@@ -79,6 +79,16 @@ class MasterViewController: UITableViewController {
     // Return false if you do not want the specified item to be editable.
     return true
   }
+
+  override func tableView(tableView: UITableView, commitEditingStyle editingStyle: UITableViewCellEditingStyle, forRowAtIndexPath indexPath: NSIndexPath) {
+    if editingStyle == .Delete {
+        objects.removeAtIndex(indexPath.row)
+        tableView.deleteRowsAtIndexPaths([indexPath], withRowAnimation: .Fade)
+    } else if editingStyle == .Insert {
+        // Create a new instance of the appropriate class, insert it into the array, and add a new row to the table view.
+    }
+  }
+
 
 }
 
