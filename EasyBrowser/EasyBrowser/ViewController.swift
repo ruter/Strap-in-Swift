e9cd54c46c2d60f43c35930dc5e2d092875e61ed
Ruter <ruter.lu@gmail.com>
2016-04-14 18:46:53 +0800

    Add options

--- EasyBrowser/EasyBrowser/ViewController.swift
+++ EasyBrowser/EasyBrowser/ViewController.swift
@@ -23,9 +23,30 @@ class ViewController: UIViewController, WKNavigationDelegate {
     super.viewDidLoad()
     // Do any additional setup after loading the view, typically from a nib.
     
+    navigationItem.rightBarButtonItem = UIBarButtonItem(title: "Open", style: .Plain, target: self, action: #selector(openTapped))
+    
     let url = NSURL(string: "https://ruter.github.io")!
     webView.loadRequest(NSURLRequest(URL: url))
     webView.allowsBackForwardNavigationGestures = true
+  }
+  
+  func openTapped() {
+    let ac = UIAlertController(title: "Open page...", message: nil, preferredStyle: .ActionSheet)
+    ac.addAction(UIAlertAction(title: "apple.com", style: .Default, handler: openPage))
+    ac.addAction(UIAlertAction(title: "ruter.github.io", style: .Default, handler: openPage))
+    ac.addAction(UIAlertAction(title: "Cancel", style: .Cancel, handler: nil))
+    
+    presentViewController(ac, animated: true, completion: nil)
+  }
+  
+  func openPage(action: UIAlertAction!) {
+    let url = NSURL(string: "https://" + action.title!)!
+    webView.loadRequest(NSURLRequest(URL: url))
+  }
+  
+  // MARK: - MKNavigationDelegate
+  func webView(webView: WKWebView, didFinishNavigation navigation: WKNavigation!) {
+    title = webView.title
   }
   
   override func didReceiveMemoryWarning() {
