6b76ae22510cf2515e758bdfb0eed551a40478bc
Ruter <ruter.lu@gmail.com>
2016-04-15 14:09:28 +0800

    Insert word possible

--- WordScramble/WordScramble/MasterViewController.swift
+++ WordScramble/WordScramble/MasterViewController.swift
@@ -7,16 +7,77 @@
 //
 
 import UIKit
+import GameplayKit
 
 class MasterViewController: UITableViewController {
 
-  var objects = [AnyObject]()
+  var allWords = [String]()
+  var objects = [String]()
 
 
   override func viewDidLoad() {
     super.viewDidLoad()
     // Do any additional setup after loading the view, typically from a nib.
     
+    navigationItem.rightBarButtonItem = UIBarButtonItem(barButtonSystemItem: .Add, target: self, action: #selector(promptForAnswer))
+    
+    if let startWordsPath = NSBundle.mainBundle().pathForResource("start", ofType: "txt") {
+      // I can't find this method /*String(contentsOfFile:, usedEncoding:)*/, but it works!??
+      if let startWords = try? String(contentsOfFile: startWordsPath, usedEncoding: nil) {
+        allWords = startWords.componentsSeparatedByString("\n")
+      }
+    } else {
+      allWords = ["silkworm"]
+    }
+    
+    startGame()
+  }
+  
+  func wordIsPossible(word: String) -> Bool {
+    return true
+  }
+  
+  func wordIsOriginal(word: String) -> Bool {
+    return true
+  }
+  
+  func wordIsReal(word: String) -> Bool {
+    return true
+  }
+  
+  func submitAnswer(answer: String) {
+    let lowerAnswer = answer.lowercaseString
+    
+    if wordIsPossible(lowerAnswer) {
+      if wordIsOriginal(lowerAnswer) {
+        if wordIsReal(lowerAnswer) {
+          objects.insert(answer, atIndex: 0)
+          
+          let indexPath = NSIndexPath(forRow: 0, inSection: 0)
+          tableView.insertRowsAtIndexPaths([indexPath], withRowAnimation: .Automatic)
+        }
+      }
+    }
+  }
+  
+  func promptForAnswer() {
+    let ac = UIAlertController(title: "Enter answer", message: nil, preferredStyle: .Alert)
+    ac.addTextFieldWithConfigurationHandler(nil)
+    
+    let submitAction = UIAlertAction(title: "Submit", style: .Default) { [unowned self, ac] _ in
+      let answer = ac.textFields![0]
+      self.submitAnswer(answer.text!)
+    }
+    ac.addAction(submitAction)
+    
+    presentViewController(ac, animated: true, completion: nil)
+  }
+  
+  func startGame() {
+    allWords = GKRandomSource.sharedRandom().arrayByShufflingObjectsInArray(allWords) as! [String]
+    title = allWords[0]
+    objects.removeAll(keepCapacity: true)
+    tableView.reloadData()
   }
 
   override func didReceiveMemoryWarning() {
@@ -37,8 +98,8 @@ class MasterViewController: UITableViewController {
   override func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
     let cell = tableView.dequeueReusableCellWithIdentifier("Cell", forIndexPath: indexPath)
 
-    let object = objects[indexPath.row] as! NSDate
-    cell.textLabel!.text = object.description
+    let object = objects[indexPath.row]
+    cell.textLabel!.text = object
     return cell
   }
 
