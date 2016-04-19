7006fc1e7da6878c4b0ee97b219b36297a65f9c7
Ruter <ruter.lu@gmail.com>
2016-04-19 11:08:27 +0800

    New project10

---
+++ Names2Faces/Names2Faces/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Names2Faces
+//
+//  Created by Ruter on 16/4/19.
+//  Copyright © 2016年 Ruter. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    // Do any additional setup after loading the view, typically from a nib.
+  }
+
+  override func didReceiveMemoryWarning() {
+    super.didReceiveMemoryWarning()
+    // Dispose of any resources that can be recreated.
+  }
+
+
+}
+
