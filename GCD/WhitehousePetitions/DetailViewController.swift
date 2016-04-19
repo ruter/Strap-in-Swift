01a30a80846f1232a329def3c5b6987ff913ca76
Ruter <ruter.lu@gmail.com>
2016-04-19 10:13:04 +0800

    Finished project 9

---
+++ GCD/WhitehousePetitions/DetailViewController.swift
@@ -0,0 +1,48 @@
+//
+//  DetailViewController.swift
+//  WhitehousePetitions
+//
+//  Created by Ruter on 16/4/17.
+//  Copyright © 2016年 Ruter. All rights reserved.
+//
+
+import UIKit
+import WebKit
+
+class DetailViewController: UIViewController {
+
+  var webView: WKWebView!
+  var detailItem: [String: String]!
+  
+  override func loadView() {
+    webView = WKWebView()
+    view = webView
+  }
+  
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    // Do any additional setup after loading the view, typically from a nib.
+    guard detailItem != nil else { return }
+    
+    if let body = detailItem["body"] {
+      var html = "<html>"
+      html += "<head>"
+      html += "<meta name=\"viewport\" content=\"width=device-width, initial-scale=1\">"
+      html += "<style> body { font-size: 150%; } </style>"
+      html += "</head>"
+      html += "<body>"
+      html += body
+      html += "</body>"
+      html += "</html>"
+      webView.loadHTMLString(html, baseURL: nil)
+    }
+  }
+
+  override func didReceiveMemoryWarning() {
+    super.didReceiveMemoryWarning()
+    // Dispose of any resources that can be recreated.
+  }
+
+
+}
+
